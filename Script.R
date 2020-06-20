d6ff1bee06441b67f148e7309daaac53b2cd2c64
Francisco Catalán <64339651+franciscoxaxo@users.noreply.github.com>
2020-06-20 00:18:51 -0400

    Update Script.R

--- Script.R
+++ Script.R
@@ -1,9 +1,10 @@
-#install.packages("tidyr")
-#install.packages("lubridate")
-#install.packages("dplyr")
-#install.packages("rvest")
-#install.packages("purrr")
-#install.packages("robotstxt")
+install.packages("tidyr")
+install.packages("lubridate")
+install.packages("dplyr")
+install.packages("rvest")
+install.packages("purrr")
+install.packages("robotstxt")
+install.packages("xml2")
 
 
 #Llamada de librerias
@@ -15,7 +16,6 @@ library(dplyr)
 library(stringr)
 library(magrittr)
 library(tidyr)
-library(ggplot2)
 library(lubridate)
 library(tibble)
 library(purrr)
@@ -26,6 +26,7 @@ Comunas <- c("SA","TA")
 Contaminantes <-c("PM10","PM25","CH4")
 fi<-"02/05/2020 1:00"
 ft<-"03/05/2020 1:00"
+
 #Fin de Imput#
 
 
@@ -34,26 +35,13 @@ ft<-"03/05/2020 1:00"
 Fecha_inicio<‐ as.POSIXct(strptime(fi, format = "%d/%m/%Y %H:%M", "GMT"))
 Fecha_termino<- as.POSIXct(strptime(ft, format = "%d/%m/%Y %H:%M", "GMT"))
 
+
 #Fechas para araña#
 Fecha_inicio_para_arana<-as.character(Fecha_inicio, format("%y%m%d"))
-Hora_inicio_para_arana<- as.character(Fecha_inicio, format( "%H%M"))
+Hora_inicio_para_arana<- as.character(Fecha_inicio, format( "%H%M")) 
 Fecha_termino_para_arana<- as.character(Fecha_termino, format("%y%m%d"))
-Hora_termino_para_arana<-as.character(Fecha_termino, format( "%H%M"))
-
-
-for(i in Fecha_inicio:Fecha_termino )
-{
-  col_fecha[i]<-Fecha_inicio++
-}
-
-
-i = NULL
-col_fecha
-
-
-#z<-gsub(" ", "",paste(a,b,d)) permite unir 3 columnas a,b y d
-
-
+Hora_termino_para_arana<-as.character(Fecha_termino, format( "%H%M")) 
+horas<-(as.numeric(Fecha_termino)/3600-as.numeric(Fecha_inicio)/3600) #horas entre fecha y fecha
 
 #Selector de arañas#
 
@@ -61,9 +49,14 @@ for(i in 1:length(Comunas))
 {
   aux<-Comunas[i]
   #Crear SA.CSV?#
-  #Columna fecha con formato fecha#
-  #Columna Comuna#
+  for(i in 1:horas) 
+  {
+    col_fecha[i]<-Fecha_inicio+3600*i
+  }
+  i = NULL
+  col_ciudad<-rep(Comunas[i],horas)
   if(aux == "SA")
+    
     {
     for(p in 1:length(Contaminantes))
     {
@@ -71,32 +64,41 @@ for(i in 1:length(Comunas))
       if(Contaminantes[p]=="PM10")
       {
         #Insertar araña PM10-Stgo Aquí#
-        #Concatenar 3 columnas#
-        #Unir a SA.CSV#
+        PM10_col1
+        PM10_col2
+        PM10_col3
+        PM10<-gsub(" ", "",paste(PM10_col1,PM10_col2,PM10_col3))
+        
         print("PM10 Stgo")
       } else
       {
         if(Contaminantes[p]=="PM25")
         {
           #Insertar araña PM25-Stgo Aquí#
-          #Concatenar 3 columnas#
-          #Unir a SA.CSV#
+          PM25_col1
+          PM25_col2
+          PM25_col3
+          PM25<-gsub(" ", "",paste(PM25_col1,PM25_col2,PM25_col3))
           print("PM25 Stgo")
         } else
         {
           if(Contaminantes[p]=="O3")
           {
             #Insertar araña O3-Stgo Aquí#
-            #Concatenar 3 columnas#
-            #Unir a SA.CSV#
+            O3_col1
+            O3_col2
+            O3_col3
+            O3<-gsub(" ", "",paste(O3_col1,O3_col2,O3_col3))
             print("O3 Stgo")
           } else
           {
             if(Contaminantes[p]=="CO")
             {
               #Insertar araña CO-Stgo Aquí#
-              #Unir 3 columnas de contaminantes#
-              #Unir a SA.CSV#
+              CO_col1
+              CO_col2
+              CO_col3
+              CO<-gsub(" ", "",paste(CO_col1,CO_col2,CO_col3))
               print("CO Stgo")
             } else{
               print(paste("Contaminante",auxiliar,"no soportado en el Software"))
@@ -105,12 +107,12 @@ for(i in 1:length(Comunas))
         }
       }
     }
-
+    data<-data.frame(col_fecha,col_ciudad,PM10,PM25,CO,O3) #Crear data frame con datos
+    write.csv(data,gsub(" ", "",paste("C:\\",Comunas[i],".CSV")), row.names = FALSE) #Crear CSV
   } else
     {
       print( paste("Comuna",aux,"no soportada en el Software"))
   }
-  
 }
 i = NULL
 p= NULL
