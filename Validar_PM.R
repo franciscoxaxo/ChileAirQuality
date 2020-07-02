27dfdd5853b8b88e57cc909217dabe4973cbfa3e
Francisco Catalán <64339651+franciscoxaxo@users.noreply.github.com>
2020-07-02 19:52:58 -0400

    Update Validar_PM.R

--- Validar_PM.R
+++ Validar_PM.R
@@ -2,7 +2,7 @@ Validar_PM<-function(){
   i = NULL
   for (i in 1:length(dataset$PM10)) 
   {
-    if((dataset$PM25[i]>dataset$PM10[i]){
+    if(dataset$PM25[i]>dataset$PM10[i]){
       dataset$PM10[i] = ""
       dataset$PM25[i] = ""
       
