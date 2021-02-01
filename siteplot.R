9417859a8b4fb109c310645ab166caed84196146
franciscoxaxo <francisco.catalan213@gmail.com>
2021-02-01 20:08:37 -0300

    generador de mapas de estaciones de monitoreo

---
+++ siteplot.R
@@ -0,0 +1,24 @@
+siteplot<-function(data){
+  library("ggplot2")
+  library("plotly")
+  library("dplyr")
+  
+  fig<-plot_ly(data,
+               lat = data$Latitud,
+               lon = data$Longitud,
+               marker = list(color = "fuchsia"),
+               hovertext = data$Estacion, 
+               type = 'scattermapbox'
+  )
+  fig<-fig %>%
+    layout(
+      mapbox = list(
+        style = 'open-street-map',
+        zoom =9,
+        center = list(lon = -70.6, lat = -33.4)
+      )
+    ) 
+  return(fig)
+}
+
+
