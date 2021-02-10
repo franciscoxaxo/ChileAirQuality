e2947c8ab7322398a3a09ffc4a5b1937cef402b0
Francisco Catalán <64339651+franciscoxaxo@users.noreply.github.com>
2021-02-10 11:35:42 -0300

    Update datamean.R

--- datamean.R
+++ datamean.R
@@ -1,8 +1,8 @@
 datamean<- function(data){
   library(data.table)
   len = length(data)
-  datamean<-data[, lapply(.SD, mean), by = .(site), .SDcols = 3:len]
+  data<- data.table(data)
+  datamean <- data[, lapply(.SD, mean, na.rm = TRUE), by = .(site), .SDcols = 3:len]
+  datamean <- as.data.frame(datamean)
   return(datamean)
 }
-
-
