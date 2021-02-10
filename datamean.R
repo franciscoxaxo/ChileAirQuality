e308d618808f5cff315ead55c0688e779ef9b784
Francisco Catalán <64339651+franciscoxaxo@users.noreply.github.com>
2021-02-10 17:01:16 -0300

    Update datamean.R

--- datamean.R
+++ datamean.R
@@ -1,8 +1,29 @@
+CV<- function(x, t = 3){
+  cv = (sd(x)/mean(x))*100
+  cv = trunc(cv, digits = t)
+  return(cv)
+}
+
+meant<-function(x, t = 3){
+  meant = trunc(mean(x), digits = t)
+  return(meant)
+}
+
+mediant<-function(x, t = 3){
+  mediant = trunc(median(x), digits = t)
+  return(mediant)
+}
+
+sdt<-function(x, t = 3){
+  sd = trunc(sd(x), digits = t)
+  return(sd)
+}
+
 datamean<- function(data, inicio = 5){
   library(data.table)
   len = length(data)
   data<- data.table(data)
-  datamean <- data[, lapply(.SD, mean, na.rm = TRUE), by = .(site, longitude, latitude), .SDcols = inicio:len]
+  datamean <- data[, lapply(.SD, meant, na.rm = TRUE), by = .(site, longitude, latitude), .SDcols = inicio:len]
   datamean <- as.data.frame(datamean)
   return(datamean)
 }
@@ -12,7 +33,7 @@ datasd<- function(data, inicio = 5){
   library(data.table)
   len = length(data)
   data<- data.table(data)
-  datasd <- data[, lapply(.SD, sd, na.rm = TRUE), by = .(site, longitude, latitude), .SDcols = inicio:len]
+  datasd <- data[, lapply(.SD, sdt, na.rm = TRUE), by = .(site, longitude, latitude), .SDcols = inicio:len]
   datasd <- as.data.frame(datasd)
   return(datasd)
 }
@@ -21,7 +42,16 @@ datamedian<- function(data, inicio = 5){
   library(data.table)
   len = length(data)
   data<- data.table(data)
-  datamedian <- data[, lapply(.SD, median, na.rm = TRUE), by = .(site, longitude, latitude), .SDcols = inicio:len]
+  datamedian <- data[, lapply(.SD, mediant, na.rm = TRUE), by = .(site, longitude, latitude), .SDcols = inicio:len]
   datamedian <- as.data.frame(datamedian)
   return(datamedian)
+}
+
+datacv<- function(data, inicio = 5){
+  library(data.table)
+  len = length(data)
+  data<- data.table(data)
+  datacv <- data[, lapply(.SD, CV, na.rm = TRUE), by = .(site, longitude, latitude), .SDcols = inicio:len]
+  datacv <- as.data.frame(datacv)
+  return(datacv)
 }
