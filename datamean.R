c1cf50111b71aa1bd7fca9c198f89d2169b11c5a
Francisco Catalán <64339651+franciscoxaxo@users.noreply.github.com>
2021-02-10 13:47:25 -0300

    Update datamean.R

--- datamean.R
+++ datamean.R
@@ -1,8 +1,27 @@
-datamean<- function(data){
+datamean<- function(data, inicio = 5){
   library(data.table)
   len = length(data)
   data<- data.table(data)
-  datamean <- data[, lapply(.SD, mean, na.rm = TRUE), by = .(site), .SDcols = 3:len]
+  datamean <- data[, lapply(.SD, mean, na.rm = TRUE), by = .(site, longitude, latitude), .SDcols = inicio:len]
   datamean <- as.data.frame(datamean)
   return(datamean)
+}
+
+
+datasd<- function(data, inicio = 5){
+  library(data.table)
+  len = length(data)
+  data<- data.table(data)
+  datasd <- data[, lapply(.SD, sd, na.rm = TRUE), by = .(site, longitude, latitude), .SDcols = inicio:len]
+  datasd <- as.data.frame(datasd)
+  return(datasd)
+}
+
+datamedian<- function(data, inicio = 5){
+  library(data.table)
+  len = length(data)
+  data<- data.table(data)
+  datamedian <- data[, lapply(.SD, median, na.rm = TRUE), by = .(site, longitude, latitude), .SDcols = inicio:len]
+  datamedian <- as.data.frame(datamedian)
+  return(datamedian)
 }
