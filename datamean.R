9549333ec900ff2b280cd92f2da38690020dbf5f
Francisco Catalán <64339651+franciscoxaxo@users.noreply.github.com>
2021-02-12 15:49:07 -0300

    Update datamean.R

--- datamean.R
+++ datamean.R
@@ -1,57 +1,58 @@
 CV<- function(x, dec = 3){
   cv = (sd(x, na.rm = TRUE)/mean(x, na.rm = TRUE))*100
-  cv = trunc(cv, digits = t)
+  cv = round(cv, dec)
+  cv = paste(cv, "%")
   return(cv)
 }
 
 meant<-function(x, dec = 3){
-  meant = trunc(mean(x, na.rm = TRUE), digits = t)
+  meant = round(mean(x, na.rm = TRUE), dec)
   return(meant)
 }
 
 mediant<-function(x, dec = 3){
-  mediant = trunc(median(x, na.rm = TRUE), digits = t)
+  mediant = round(median(x, na.rm = TRUE), dec)
   return(mediant)
 }
 
 sdt<-function(x, dec = 3){
-  sd = trunc(sd(x, na.rm = TRUE), digits = t)
+  sd = round(sd(x, na.rm = TRUE), dec)
   return(sd)
 }
 
-datamean<- function(data, col = 5){
+datamean<- function(data, inicio = 5){
   library(data.table)
   len = length(data)
   data<- data.table(data)
-  datamean <- data[, lapply(.SD, meant), by = .(site, longitude, latitude), .SDcols = col:len]
+  datamean <- data[, lapply(.SD, meant), by = .(site, longitude, latitude), .SDcols = inicio:len]
   datamean <- as.data.frame(datamean)
   return(datamean)
 }
 
 
-datasd<- function(data, col = 5){
+datasd<- function(data, inicio = 5){
   library(data.table)
   len = length(data)
   data<- data.table(data)
-  datasd <- data[, lapply(.SD, sdt), by = .(site, longitude, latitude), .SDcols = col:len]
+  datasd <- data[, lapply(.SD, sdt), by = .(site, longitude, latitude), .SDcols = inicio:len]
   datasd <- as.data.frame(datasd)
   return(datasd)
 }
 
-datamedian<- function(data, col = 5){
+datamedian<- function(data, inicio = 5){
   library(data.table)
   len = length(data)
   data<- data.table(data)
-  datamedian <- data[, lapply(.SD, mediant), by = .(site, longitude, latitude), .SDcols = col:len]
+  datamedian <- data[, lapply(.SD, mediant), by = .(site, longitude, latitude), .SDcols = inicio:len]
   datamedian <- as.data.frame(datamedian)
   return(datamedian)
 }
 
-datacv<- function(data, col = 5){
+datacv<- function(data, inicio = 5){
   library(data.table)
   len = length(data)
   data<- data.table(data)
-  datacv <- data[, lapply(.SD, CV), by = .(site, longitude, latitude), .SDcols = col:len]
+  datacv <- data[, lapply(.SD, CV), by = .(site, longitude, latitude), .SDcols = inicio:len]
   datacv <- as.data.frame(datacv)
   return(datacv)
 }
