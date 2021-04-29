92ac75baf30cce35be36480d0957e190de6f2d11
Francisco Catalán <64339651+franciscoxaxo@users.noreply.github.com>
2021-04-29 01:29:34 -0400

    Update datamean.R

--- datamean.R
+++ datamean.R
@@ -123,3 +123,26 @@ comparFunction<- function(data){
   print(a)
   return(a)
 }
+
+siteplot<-function(data, latitud = data$Longitud, longitud = data$Latitud, centro = c(-70.6, -33.4)){
+  library("ggplot2")
+  library("plotly")
+  library("dplyr")
+  
+  fig<-plot_ly(data,
+               lat = latitud,
+               lon = longitud,
+               marker = list(color = "red"),
+               hovertext = ~paste("Estacion:", data$Estacion,"<br />", "Site:", data$Ciudad), 
+               type = 'scattermapbox'
+  )
+  fig<-fig %>%
+    layout(
+      mapbox = list(
+        style = 'open-street-map',
+        zoom =9,
+        center = list(lon = centro[1], lat = centro[2])
+      )
+    ) 
+  return(fig)
+}
