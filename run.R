b2bea23367f33b627457182745550798831b57a0
Francisco Catalán <64339651+franciscoxaxo@users.noreply.github.com>
2021-04-29 01:36:31 -0400

    Delete run.R

--- run.R
+++
@@ -1,5 +0,0 @@
-library(rsconnect)
-rsconnect::setAccountInfo(name='chileairquality',
-                          token='5E6958FD7D9EF6E45D6377160AD605AF',
-                          secret='5ubXakgNhAMP0w0Sba6TlET0TR4z3+Xb/yplttfZ')
-rsconnect::deployApp("~/R/ChileAirQualityProject/ChileAirQuality")
